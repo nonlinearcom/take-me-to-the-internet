32a3e6f001bf418ea4ec26539cb3cdb159c4700e
nonlinearcom <manuel@non-linear.com>
2025-01-02 22:56:20 -0500

    TMTTI logo refactor

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,9 +1,6 @@
 <template>
   <header class="app-header">
-    <h1 class="header__title">
-      <span>TAKE</span><span>ME</span><span>TO</span><span>THE</span><span>INTERNET</span>
-    </h1>
-
+    <HeaderLogo />
     <nav class="main-menu">
       <NuxtLink
         to="/articles"
@@ -49,6 +46,9 @@
 </template>
 
 <script setup lang="ts">
+defineOptions({
+  name: 'AppHeader',
+})
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
@@ -58,43 +58,7 @@ const toggleDark = useToggle(isDark)
   display: grid;
   grid-template-columns: 50% 1fr 1fr 40px;
   gap: 32px;
-  align-items: flex-start;
   padding: var(--app-margin-small);
-
-  .header__title {
-    margin-bottom: 0;
-
-    /* dynamic logo */
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    margin-right: 50px;
-    font-size: var(--title);
-    span {
-      padding: 0 8px;
-      transition: background-color 0.3s;
-      background-color: var(--bg-color);
-
-      &:first-child {
-        padding-left: 0;
-      }
-
-      &:last-child {
-        padding-right: 0;
-      }
-    }
-    &:before {
-      content: '';
-      z-index: -1;
-      position: absolute;
-      display: block;
-      height: 2px;
-      top: 50%;
-      right: 2px;
-      left: 2px;
-      background-color: var(--text-color);
-    }
-  }
 
   .main-menu {
     grid-column: 3;
@@ -107,14 +71,15 @@ const toggleDark = useToggle(isDark)
 
   .mode-toggle {
     grid-column: 4;
+    align-self: flex-start;
   }
 }
 
 @media (max-width: 1024px) {
   .app-header {
-    grid-template-columns: 50% 1fr auto 40px;
+    grid-template-columns: 50% 1fr;
     gap: 20px;
-    .header__title {
+    .tmtti-logo {
       font-size: var(--text);
     }
   }
@@ -122,20 +87,23 @@ const toggleDark = useToggle(isDark)
 
 @media (max-width: 768px) {
   .app-header {
-    grid-template-columns: 3fr auto 40px;
+    grid-template-columns: 1fr;
+
     padding: var(--app-margin-mini);
 
-    .header__title {
-      grid-column: 1 / span 3;
-      grid-row: 1;
-      margin-right: 0;
-      margin-bottom: 25px;
+    .tmtti-logo {
+      grid-column: 1;
+    }
+
+    .main-menu {
+      grid-column: 1;
+      grid-row: 2;
     }
 
     .mode-toggle {
-      grid-column: 3;
+      justify-self: flex-end;
+      grid-column: 1;
       grid-row: 2;
-      transform: translate(8px, -6px);
     }
   }
 }
---
+++ components/header/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <NuxtLink to="/">
+    <h1 class="tmtti-logo">
+      TAKE<span class="dash" />ME<span class="dash" />TO<span class="dash" />THE<span class="dash" />INTERNET
+    </h1>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="postcss">
+  /* Dynamic logo */
+.tmtti-logo {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  gap: 6px;
+  font-size: var(--title);
+  transition: background-color 0.3s;
+
+  .dash {
+    height: 2px;
+    min-width: 12px;
+    top: 50%;
+    background-color: var(--text-color);
+    flex: 1 0 auto;
+    align-self: center;
+    border-radius: 1px;
+  }
+}
+</style>
